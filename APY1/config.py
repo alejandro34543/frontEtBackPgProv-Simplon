e691789bef1d11337c94b85829d53a8a8689f262
John Armitage <john.joseph.armitage@gmail.com>
2019-12-05 10:59:30 +0100

    still not working - latest version

--- APY1/config.py
+++ APY1/config.py
@@ -6,11 +6,11 @@ John Armitage 3/12/2019
 """
 import os
 
-postgres_user = os.environ['POST_USER']
-postgres_pass = os.environ['POST_PASSWORD']
-uri = os.environ['URI_POSTGRES']
-port = os.environ['POST_PORT']
-dbase = os.environ['DBASE']
+postgres_user = os.environ['POST_USER'].rstrip()
+postgres_pass = os.environ['POST_PASSWORD'].rstrip()
+uri = os.environ['URI_POSTGRES'].rstrip()
+port = os.environ['POST_PORT'].rstrip()
+dbase = os.environ['DBASE'].rstrip()
 
 #DATABASE_URI = 'postgres+psycopg2://postgres:WRMNSXGol1@a8f213870167411ea9df302af6eaa18b-1639397014.eu-west-1.elb.amazonaws.com:5432/John-axel'
 DATABASE_URI = 'postgres+psycopg2://{}:{}@{}:{}/{}'.format(postgres_user, postgres_pass, uri, port, dbase)
